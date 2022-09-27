5c77d69402ed52409bc5418932bdd7d12e838307
Adithya Rathnayake <adithya0076@gmail.com>
2022-09-27 15:27:40 +0530

    Update settings.py

--- elaris/settings.py
+++ elaris/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +150,6 @@ EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'contact@pdelaris.com' 
 EMAIL_HOST_PASSWORD = 'Nuwangi1!'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
